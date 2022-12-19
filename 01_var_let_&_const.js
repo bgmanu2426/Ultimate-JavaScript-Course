2cc39c315faf5deab41a98d86c6a6dd95810597d
Manubg <bgmanu2426@gmail.com>
2022-12-19 07:10:55 +0530

    chapter-1

---
+++ 01_var_let_&_const.js
@@ -0,0 +1,36 @@
+// var is globally scoped, it can be re-declared and also be updated
+var a = "Manu BG";
+console.log(a);
+{
+    var a = 2580;  // re-declared in the block
+    console.log(a);
+}
+console.log(a); // the variabled updated globally
+var a = "Changed"; // re-declared and updated
+console.log(a);
+
+// let is block scoped, it cannot be re-declared but can be updated
+let b = 25847;
+console.log(b);
+{
+    let b = "In the block"; //redeclared and can be updated in the block
+    console.log(b);
+}
+console.log(b);
+/* let b = "Not changed" // cannot be redeclared
+console.log(b); */
+b = "Updated"; // can be updated
+console.log(b);
+
+// const is block scoped, it cannot be re-declared and updated
+const c = "This cannot be changed or updated";
+console.log(c);
+{
+    const c = "Changed";  //redeclared and can be updated in the block
+    console.log(c);
+}
+console.log(c);
+/* let c = 45841; // const cannot be redeclared
+console.log(c); */
+/* c = null; // cannot be updated
+console.log(c); */
---
+++ 02_data_types.js
@@ -0,0 +1,36 @@
+/* There are 2 types of data types they are :-
+i) Primitive Data Type
+    • Boolean
+    • BigInt
+    • Number
+    • Null
+    • String
+    • Symbol
+    • Undefined
+    // Remembered as BBNNSSU
+ii) Objects (Non-primitive data types) 
+    */
+
+let a = true; // or false
+let b = BigInt(45);
+let c = 454;
+let d = null;
+let e = "Hello there it is a string";
+let f = Symbol(45);
+let g;
+console.log(a , b, c , d, e, f, g);
+console.log(typeof(a), typeof(b), typeof(c), typeof(d), typeof(e), typeof(f), typeof(g));
+
+/****************************************************************************************************/
+let Objects1 = {
+    name: "test",
+    institute: "GPT hrr",
+    reg_no: "170EC20027"
+}
+console.log(Objects1);
+console.log(Objects1["name"]);
+console.log(Objects1["institute"]);
+console.log(Objects1["reg_no"]);
+
+Objects1["name"] = "Manu BG";
+console.log(Objects1);
